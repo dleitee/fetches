c74e259e5b32f3ff200f2734b6f133f6a18cdb14
Daniel Leite de Oliveira <dleitee@gmail.com>
2018-05-22 13:14:52 -0300

    feat(client): Add request type into client.

--- src/client.js
+++ src/client.js
@@ -1,8 +1,11 @@
 import deepmerge from 'deepmerge'
 
+import NOT_IMPLEMENTED from './utils/not-implemented'
 import normalizeURL from './utils/normalize-url'
 
 const DEFAULT_OPTIONS = {
+  requestType: 'json',
+  request: {},
   uri: {
     removeTrailingSlash: false,
   },
@@ -12,10 +15,23 @@ export class Client {
   constructor(uri, options = {}) {
     this.options = deepmerge(DEFAULT_OPTIONS, options)
     this.uri = normalizeURL(uri)(this.options.uri)
+
+    if (this.options.requestType !== DEFAULT_OPTIONS.requestType) {
+      NOT_IMPLEMENTED()
+    }
+    this.requestType = this.options.requestType
   }
 
   getURI() {
     return this.uri
+  }
+
+  static appendToURI(client, uri = '') {
+    return normalizeURL([client.getURI(), uri])(client.options.uri)
+  }
+
+  getRequestType() {
+    return this.requestType
   }
 }
 
