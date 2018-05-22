7ff4a671789ed976f5e2abdda3a5cd70ea3ffa7e
Daniel Leite de Oliveira <dleitee@gmail.com>
2018-05-22 20:59:25 -0300

    feat(compact-object): Use Object assign instead of object destructuring.

--- src/utils/compact-object.js
+++ src/utils/compact-object.js
@@ -1,8 +1,13 @@
 const isUndefined = value => typeof value === 'undefined'
 
-export default raw => raw && Object.keys(raw)
-  .filter(key => !isUndefined(raw[key]))
-  .reduce((obj, key) => ({
-    ...obj,
-    [key]: raw[key],
-  }), {})
+export default raw =>
+  raw &&
+  Object.keys(raw)
+    .filter(key => !isUndefined(raw[key]))
+    .reduce(
+      (obj, key) =>
+        Object.assign({}, obj, {
+          [key]: raw[key],
+        }),
+      {}
+    )
