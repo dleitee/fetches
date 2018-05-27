1d0c0bdfe077475f65cb440babaea200139fc3c9
Daniel Leite de Oliveira <dleitee@gmail.com>
2018-05-27 09:15:18 -0300

    feat(npm): Add .npmignore file

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import babel from 'rollup-plugin-babel'
 export default {
   input: 'src/index.js',
   output: {
-    file: 'build/bundle.js',
+    file: 'lib/module.js',
     format: 'cjs',
   },
   plugins: [
